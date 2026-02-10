54ee4d7a08b3f756dfe5d0cf138bcb394a189c28
sastrearnau <arnausastregp@gmail.com>
2026-02-10 16:19:45 +0100

    Update router.py

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -5,5 +5,6 @@ from app.api.v1.endpoints import bkt
 api_router = APIRouter()
 api_router.include_router(documents.router, prefix="/documents", tags=["documents"])
 api_router.include_router(quizzes.router, prefix="/quizzes", tags=["quizzes"])
-router.include_router(bkt.router, prefix="/bkt", tags=["bkt"])
+api_router.include_router(bkt.router, prefix="/bkt", tags=["bkt"])
+
 
